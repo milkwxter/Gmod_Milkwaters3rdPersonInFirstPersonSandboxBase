642f2d8f3c455c691f1acfab6d4310ad07c7b2b8
milkwxter <ethan.r.bates2003@gmail.com>
2026-01-28 14:14:30 -0600

    Rocket launchers

---
+++ milkwaters_thirdperson_base/lua/entities/mw_rocket_proj.lua
@@ -0,0 +1,146 @@
+if SERVER then
+	AddCSLuaFile()
+	game.AddParticles( "particles/explosion.pcf" )
+	PrecacheParticleSystem("ExplosionCore_MidAir")
+end
+
+DEFINE_BASECLASS("base_anim")
+
+ENT.Type = "anim"
+ENT.PrintName = "MW Rocket"
+ENT.Spawnable = false
+
+local impactEffect = "ExplosionCore_MidAir"
+local trailEffect = "rockettrail"
+
+function ENT:Initialize()
+    if SERVER then
+        self:SetModel("models/weapons/w_models/w_rocket.mdl")
+
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        self:SetSolid(SOLID_VPHYSICS)
+        self:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
+		
+		ParticleEffectAttach(trailEffect, PATTACH_ABSORIGIN_FOLLOW, self, 0)
+
+        local phys = self:GetPhysicsObject()
+        if IsValid(phys) then
+            phys:Wake()
+            phys:SetMass(2)
+        end
+
+        self.Radius = 100
+    end
+end
+
+function ENT:PhysicsCollide(data, phys)
+	if self.Exploded then return end
+	self:Detonate(data.HitPos, data.HitNormal)
+end
+
+function ENT:StartTouch(ent)
+	if self.Exploded then return end
+	if ent:IsPlayer() or ent:IsNPC() then
+		self:Detonate(self:GetPos(), Vector(0,0,1))
+	end
+end
+
+function ENT:Detonate(pos, normal)
+	if self.Exploded then return end
+	self.Exploded = true
+	
+	-- client effect
+	ParticleEffect(impactEffect, pos, normal:Angle(), nil)
+	
+	-- damage
+	self:DoExplosionDamage(pos)
+	
+	self:Remove()
+end
+
+function ENT:DoExplosionDamage(pos)
+    local attacker = self:GetOwner()
+    if not IsValid(attacker) then attacker = self end
+
+    local entities = ents.FindInSphere(pos, self.Radius)
+	
+	-- get the weapon that fired the rocket
+	local wep = attacker.GetActiveWeapon and attacker:GetActiveWeapon()
+	local dmgAmount = wep.Primary.Damage
+
+    for _, ent in ipairs(entities) do
+        if ent:IsPlayer() or ent:IsNPC() then
+            local dist = ent:GetPos():Distance(pos)
+            local frac = math.Clamp(1 - (dist / self.Radius), 0, 1)
+
+            -- half damage at edge
+            local damage = Lerp(frac, dmgAmount * 0.5, dmgAmount)
+			
+			-- modify the damage more
+			if IsValid(wep) and wep.ModifyDamage then
+				local dmginfo = DamageInfo()
+				dmginfo:SetDamage(damage)
+				
+				local fakeTr = {
+					Entity = ent,
+					HitPos = ent:GetPos(),
+					HitNormal = Vector(0,0,1)
+				}
+
+				damage, isMiniCrit, isFullCrit = wep:ModifyDamage(attacker, fakeTr, dmginfo)
+				
+				print(isFullCrit and 2 or (isMiniCrit and 1 or 0))
+				
+				-- increase damage based on crits
+				if isFullCrit then
+					damage = damage * 3
+				elseif isMiniCrit then
+					damage = damage * 1.35
+				end
+			end
+
+            -- apply damage
+            local dmg = DamageInfo()
+            dmg:SetDamage(damage)
+            dmg:SetDamageType(DMG_BLAST)
+            dmg:SetAttacker(attacker)
+            dmg:SetInflictor(self)
+            dmg:SetDamagePosition(pos)
+
+            local oldHP = ent:Health()
+            ent:TakeDamageInfo(dmg)
+		
+			-- add time of hit for the damage numbers hook (trust me)
+			ent._MW_LastHit = {attacker = attacker, crit = isFullCrit and 2 or (isMiniCrit and 1 or 0), timeHit = CurTime()}
+			
+			-- send damage sound
+			if SERVER and IsValid(attacker) and attacker:IsPlayer() then
+				net.Start("mw_damage_sound")
+				net.WriteUInt(isFullCrit and 2 or (isMiniCrit and 1 or 0), 2)
+				net.Send(attacker)
+			end
+			
+			-- knockback
+			local dir = (ent:GetPos() - pos):GetNormalized()
+			
+			local force = frac * 600
+
+			-- self blast jump multiplier
+			if ent == attacker then
+				force = force * 1.4
+			end
+
+			-- apply to players
+			if ent:IsPlayer() then
+				ent:SetVelocity(ent:GetVelocity() + dir * force)
+			end
+
+            -- gibbing logic
+            if ent:IsPlayer() and (damage > oldHP + 10) then
+                ent:EmitSound("physics/flesh/flesh_bloody_break.wav")
+                -- you can spawn gibs here if you want
+            end
+        end
+    end
+end
---
+++ milkwaters_thirdperson_base/lua/weapons/mw_rocket_launcher.lua
@@ -0,0 +1,38 @@
+if SERVER then AddCSLuaFile() end
+
+SWEP.Base = "milkwaters_3p_base"
+
+SWEP.PrintName = "Rocket Launcher"
+SWEP.Purpose = "A standard rocket launcher."
+SWEP.Category = "Milkwater"
+SWEP.Spawnable = true
+SWEP.IconOverride = "weapons/mw_rocket_launcher.png"
+
+SWEP.WorldModel = "models/weapons/c_models/c_rocketlauncher/c_rocketlauncher.mdl"
+
+SWEP.HandOffset_Pos = Vector(4, -1, -3) -- forward, right, up
+SWEP.HandOffset_Ang = Angle(10, 0, 180) -- pitch, yaw, roll
+
+SWEP.MuzzleOffset_Pos = Vector(0, 0, 7) -- forward, right, up
+SWEP.MuzzleOffset_Ang = Angle(0, 0, 0) -- pitch, yaw, roll
+SWEP.MuzzleEffect = ""
+
+SWEP.SoundShootPrimary = "weapons/rocket_shoot.wav"
+SWEP.HoldType = "rpg"
+SWEP.Caseless = true
+
+SWEP.Primary.ClipSize = 4
+SWEP.Primary.DefaultClip = 4
+SWEP.Primary.Ammo = "Pistol"
+
+SWEP.Projectile = true
+SWEP.ProjectileClass = "mw_rocket_proj"
+SWEP.ProjectileSpeed = 1100
+SWEP.ProjectileGravity = false
+
+SWEP.Primary.Automatic = true
+SWEP.Primary.FireDelay = 0.8
+SWEP.Primary.Damage = 90
+SWEP.Primary.NumShots = 1
+SWEP.Cone = 0.05
+SWEP.Primary.Recoil = 1
