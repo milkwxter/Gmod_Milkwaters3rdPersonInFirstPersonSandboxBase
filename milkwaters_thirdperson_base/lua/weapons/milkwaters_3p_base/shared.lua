f25a3f10e327f51071a4a22d1c1af055d1be9ed7
milkwxter <ethan.r.bates2003@gmail.com>
2026-02-01 20:26:11 -0600

    Added butterfly knife and fixed some melee logic

--- milkwaters_thirdperson_base/lua/weapons/milkwaters_3p_base/shared.lua
+++ milkwaters_thirdperson_base/lua/weapons/milkwaters_3p_base/shared.lua
@@ -702,30 +702,6 @@ function SWEP:DoMeleeTrace()
 
     local damage = self.MeleeDamage
     local isMiniCrit, isFullCrit = false, false
-
-    -- apply crit modifiers
-    if SERVER and IsValid(hit) and hit:IsPlayer() then
-        local dmginfo = DamageInfo()
-        dmginfo:SetDamage(damage)
-
-        damage, isMiniCrit, isFullCrit = self:ModifyDamage(owner, tr, dmginfo)
-
-        if isFullCrit then
-            damage = damage * 3
-        elseif isMiniCrit then
-            damage = damage * 1.35
-        end
-    end
-
-    -- add time of hit for the damage numbers hook (trust me)
-	hit._MW_LastHit = {attacker = owner, crit = isFullCrit and 2 or (isMiniCrit and 1 or 0), timeHit = CurTime()}
-	
-	-- send damage sound
-	if SERVER and IsValid(owner) and owner:IsPlayer() then
-		net.Start("mw_damage_sound")
-		net.WriteUInt(isFullCrit and 2 or (isMiniCrit and 1 or 0), 2)
-		net.Send(owner)
-	end
 	
 	-- bullet for effects and damage
 	if SERVER then
@@ -741,12 +717,52 @@ function SWEP:DoMeleeTrace()
 		bullet.Force    = damage * 0.5
 		bullet.Damage   = damage
 		bullet.Distance = self.MeleeRange + 32
+		
+		bullet.Callback = function(att, tr, dmginfo)
+			if CLIENT and not IsFirstTimePredicted() then return end
+
+			local hit = tr.Entity
+
+			-- apply damage modifiers
+			local newDamage
+			if SERVER and IsValid(att) and att:IsPlayer() then
+				if IsValid(hit) then
+					newDamage, isMiniCrit, isFullCrit = self:ModifyDamage(att, tr, dmginfo)
+					
+					-- increase damage based on crits
+					if isFullCrit then
+						newDamage = newDamage * 3
+					elseif isMiniCrit then
+						newDamage = newDamage * 1.35
+					end
+					
+					-- finish the calcs
+					dmginfo:SetDamage(newDamage)
+					
+					-- add time of hit for the damage numbers hook (trust me)
+					hit._MW_LastHit = {attacker = att, crit = isFullCrit and 2 or (isMiniCrit and 1 or 0), timeHit = CurTime()}
+					
+					-- perform a magic extra effect
+					self:ExtraEffectOnHit(att, tr)
+				end
+			end
+		end
 
 		owner:FireBullets(bullet)
 	end
 
     -- hit sound
     owner:EmitSound(self.MeleeHitSound)
+	
+	-- add time of hit for the damage numbers hook (trust me)
+	hit._MW_LastHit = {attacker = owner, crit = isFullCrit and 2 or (isMiniCrit and 1 or 0), timeHit = CurTime()}
+	
+	-- send damage sound
+	if SERVER and IsValid(owner) and owner:IsPlayer() then
+		net.Start("mw_damage_sound")
+		net.WriteUInt(isFullCrit and 2 or (isMiniCrit and 1 or 0), 2)
+		net.Send(owner)
+	end
 
     -- custom effect hook
     self:ExtraEffectOnHit(owner, tr)
---
+++ milkwaters_thirdperson_base/lua/weapons/mw_butterfly_knife.lua
@@ -0,0 +1,62 @@
+if SERVER then AddCSLuaFile() end
+
+SWEP.Base = "milkwaters_3p_base"
+
+SWEP.PrintName = "Butterfly Knife"
+SWEP.Purpose = "Backstab for crits!"
+SWEP.Category = "Milkwater"
+SWEP.Spawnable = true
+SWEP.IconOverride = "weapons/mw_butterfly_knife.png"
+
+SWEP.WorldModel = "models/weapons/c_models/c_knife/c_knife.mdl"
+
+SWEP.HandOffset_Pos = Vector(3, -1, -2) -- forward, right, up
+SWEP.HandOffset_Ang = Angle(0, 0, 180) -- pitch, yaw, roll
+
+SWEP.MuzzleOffset_Pos = Vector(20, -1, 4) -- forward, right, up
+SWEP.MuzzleOffset_Ang = Angle(0, 0, 0) -- pitch, yaw, roll
+SWEP.MuzzleEffect = ""
+
+SWEP.SoundShootPrimary = ""
+SWEP.HoldType = "knife"
+SWEP.Casing = ""
+
+SWEP.Primary.ClipSize = 0
+SWEP.Primary.DefaultClip = 0
+SWEP.Primary.Ammo = "none"
+
+SWEP.Primary.Automatic = true
+SWEP.Primary.FireDelay = 0.8
+SWEP.Primary.Damage = 0
+SWEP.Primary.NumShots = 1
+SWEP.Cone = 0
+SWEP.Primary.Recoil = 1
+
+SWEP.Melee = true
+SWEP.MeleeDamage = 40
+SWEP.MeleeRange = 70
+SWEP.MeleeDelay = 0.2
+
+-- full crits if you got a back stab
+function SWEP:ModifyDamage(att, tr, dmginfo)
+    -- get base damage + base crits
+    local dmg, isMiniCrit, isFullCrit = self.BaseClass.ModifyDamage(self, att, tr, dmginfo)
+
+    local hit = tr.Entity
+    if not IsValid(hit) then
+        return dmg, isMiniCrit, isFullCrit
+    end
+
+    if hit:IsPlayer() or hit:IsNPC() then
+		local attackerForward = att:GetAimVector()
+		local victimForward = hit:GetAimVector()
+		-- dot > 0.5 means attacker is behind victim and facing same direction
+		local dot = attackerForward:Dot(victimForward)
+		if dot > 0.5 then
+			isFullCrit = true
+			dmg = 150
+		end
+	end
+
+    return dmg, isMiniCrit, isFullCrit
+end
