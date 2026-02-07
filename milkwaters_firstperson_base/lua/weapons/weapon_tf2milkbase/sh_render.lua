be78a5704c08224aea2ce87135b999915dfccd04
milkwxter <ethan.r.bates2003@gmail.com>
2026-02-06 19:08:58 -0600

    Bruhhhh

---
+++ milkwaters_firstperson_base/lua/weapons/weapon_tf2milkbase/sh_render.lua
@@ -0,0 +1,43 @@
+-- sh_render.lua
+function SWEP:DrawWorldModel()
+	if CLIENT and self:GetZoomed() then return end
+	
+	local mdl = self.GetCurrentWorldModel and self:GetCurrentWorldModel() or self.WorldModel
+	
+	if mdl and (not IsValid(self.WModel) or self.WModel:GetModel() ~= mdl) then
+		if IsValid(self.WModel) then
+			self.WModel:Remove()
+		end
+		
+		self.WModel = ClientsideModel(mdl)
+		self.WModel:SetNoDraw(true)
+	end
+	
+    if not IsValid(self.WModel) then return end
+
+    local owner = self:GetOwner()
+
+    if IsValid(owner) then
+        local boneid = owner:LookupBone("ValveBiped.Bip01_R_Hand")
+        if boneid then
+            local matrix = owner:GetBoneMatrix(boneid)
+            if matrix then
+                local pos, ang = LocalToWorld(
+                    self.HandOffset_Pos or vector_origin,
+                    self.HandOffset_Ang or angle_zero,
+                    matrix:GetTranslation(),
+                    matrix:GetAngles()
+                )
+				
+				-- clientside model
+                self.WModel:SetPos(pos)
+                self.WModel:SetAngles(ang)
+				self.WModel:DrawModel()
+				
+				-- serverside model
+				self:SetRenderOrigin(pos)
+				self:SetRenderAngles(ang)
+            end
+        end
+    end
+end
